817f9af0e07755a11572530d1666d9397f08cb12
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-11 23:22:10 +0100

    adds dynamically populated list to display function

--- src/views/display_view.js
+++ src/views/display_view.js
@@ -13,9 +13,25 @@ DisplayView.prototype.bindEvents = function () {
 
 DisplayView.prototype.display = function (familyObject) {
   this.container.innerHTML = '';
+
   const displayHeader = document.createElement('h2');
   displayHeader.textContent = `${familyObject.name}`;
   this.container.appendChild(displayHeader);
+
+  const displayDescription = document.createElement('p');
+  displayDescription.textContent = `${familyObject.description}`;
+  this.container.appendChild(displayDescription);
+
+  const displayExamples = document.createElement('ul');
+  displayExamples.textContent = `Examples of ${familyObject.name} instruments include...`
+  displayExamples.id = 'instrument-list';
+  this.container.appendChild(displayExamples);
+  const list = document.querySelector('#instrument-list');
+  familyObject.instruments.forEach( (instrument) => {
+    const entry = document.createElement('li');
+    entry.textContent = instrument;
+    list.appendChild(entry);
+  })
 
 };
 
