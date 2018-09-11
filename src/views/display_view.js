e96e18a77b06ab5f0a3e6cdadfc0cbaa0a6e7ab7
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-11 22:48:57 +0100

    adds channel family ready

--- src/views/display_view.js
+++ src/views/display_view.js
@@ -1,9 +1,15 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const DisplayView = function () {
-
+const DisplayView = function (container) {
+  this.container = container;
 };
 
+DisplayView.prototype.bindEvents = function () {
+  PubSub.subscribe('InstrumentFamilies:chosen-family-ready', (event) => {
+    const family = event.detail;
+    console.log(family);
+  })
+};
 
 
 module.exports = DisplayView;
